c4d02d7cfb150ead0f21ad8d8b2f1f64c003f646
MitchellKluesner <31300693+MitchellKluesner@users.noreply.github.com>
2022-07-06 12:58:49 -0700

    v1.0.10 update
    v1.0.10 update

--- dependencies.R
+++ dependencies.R
@@ -20,12 +20,5 @@ install.packages("yaml")
 # Updated 4.7.19 due to error with Bioconductor packages
 # Updated again 4.30.19
 
-# Run this code chunk in terminal first
-#
-# options(repos = BiocManager::repositories())
-# source("https://bioconductor.org/biocLite.R")
-#
-# rsconnect::deployApp("/Users/kluesner/Desktop/Research/spliceR/apps/SpliceR")
-
 BiocManager::install("Biostrings")
 BiocManager::install("sangerseqR")
